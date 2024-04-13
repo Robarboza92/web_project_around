b6cfe7cf672cb887a8c18241ab36e79ef6b026ea
Rodrigo <rodrigobarboza@outlook.com>
2024-04-13 14:57:19 -0300

    conclusão JS

--- blocks/script.js
+++ blocks/script.js
@@ -1,1 +1,58 @@
-console.log("funcionando");
+const popupForm = document.querySelector(".popup");
+const editButton = document.querySelector(".profile__info__edit-button");
+const closeButton = document.querySelector(".popup__close-button");
+
+const nameInput = document.querySelector(".popup__name-input");
+const jobInput = document.querySelector(".popup__job-input");
+
+const profileName = document.querySelector(".profile__info__name");
+const profileJob = document.querySelector(".profile__info__job");
+
+const saveButton = document.querySelector(".popup__save-button");
+
+function changeDisplaytoFlex() {
+  popupForm.classList.add("popup_change_display");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+function changeDisplaytoNone() {
+  popupForm.classList.remove("popup_change_display");
+}
+
+editButton.addEventListener("click", changeDisplaytoFlex);
+closeButton.addEventListener("click", changeDisplaytoNone);
+
+function SaveProfileFormSubmit(evento) {
+  evento.preventDefault();
+  if (nameInput.value == "" && jobInput.value != "") {
+    alert("Preencha seu nome!");
+    return;
+  }
+  if (jobInput.value == "") {
+    alert("Preencha sua profissão!");
+    return;
+  }
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  changeDisplaytoNone();
+}
+
+saveButton.addEventListener("click", SaveProfileFormSubmit);
+// Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
+// Explicaremos em mais detalhes posteriormente.
+
+// Vamos encontrar os campos de formulário do DOM
+/*const nameInput = // Use querySelector()
+    let jobInput = // Use querySelector()
+
+    // Pegue os valores de cada campo do valor da propriedade correspondente
+
+    // Selecione os elementos aos quais os valores dos campos serão inseridos
+
+    // Insira novos valores usando a
+    // propriedade textContent
+}
+
+// Conecte o handler ao formulário:
+// ele vai observar o evento de submit
+formElement.addEventListener('submit', handleProfileFormSubmit);*/

36e1cd92298b79a5cb5fe045549ed4f47d2dc334
Rodrigo Barboza <rodrigo@placon.local>
2024-04-30 18:55:58 -0300

    inicio projeto sprint 6

--- blocks/script.js
+++ blocks/script.js
@@ -38,3 +38,44 @@ function SaveProfileFormSubmit(evento) {
 }
 
 saveButton.addEventListener("click", SaveProfileFormSubmit);
+
+const popupForm = document.querySelector(".popup");
+const addButton = document.querySelector(".card__addbuttonn");
+const closeButton = document.querySelector(".popup__close-button");
+
+const titleInput = document.querySelector(".popup__title-input");
+const linkInput = document.querySelector(".popup__link-input");
+
+// const profileName = document.querySelector(".profile__info-name");
+// const profileJob = document.querySelector(".profile__info-job");
+
+const createButton = document.querySelector(".popup__create-button");
+
+function changeDisplaytoFlex() {
+  popupForm.classList.add("popup_change_display");
+  titleInput.value = profiletitle.textContent;
+  linkInput.value = profilelink.textContent;
+}
+function changeDisplaytoNone() {
+  popupForm.classList.remove("popup_change_display");
+}
+
+addButton.addEventListener("click", changeDisplaytoFlex);
+closeButton.addEventListener("click", changeDisplaytoNone);
+
+// function SaveProfileFormSubmit(evento) {
+//   evento.preventDefault();
+//   if (nameInput.value == "" && jobInput.value != "") {
+//     alert("Preencha seu nome!");
+//     return;
+//   }
+//   if (jobInput.value == "") {
+//     alert("Preencha sua profissão!");
+//     return;
+//   }
+//   profileName.textContent = nameInput.value;
+//   profileJob.textContent = jobInput.value;
+//   changeDisplaytoNone();
+// }
+
+// saveButton.addEventListener("click", SaveProfileFormSubmit);

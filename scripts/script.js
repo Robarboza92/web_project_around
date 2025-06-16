c8bc07059cb9d68f7dfb80d043bcb5a8f5253a14
rodrigobarboza <rodrigobarboza@outlook.com>
2025-06-16 19:08:22 -0300

    corrigido overlay

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,26 @@
-let body = document.querySelector(".body")
-let popup = body.querySelector(".popup")
-let profilebutton = body.querySelector(".profile__info-edit")
-let closebutton = body.querySelector(".popup__close-button")
-let profileName = body.querySelector(".profile__name")
-let profileJob = body.querySelector(".profile__job")
-let nameInput = body.querySelector(".popup__name-input")
-let jobInput = body.querySelector(".popup__job-input")
-let formElement = body.querySelector(".popup.container")
-let saveButton = body.querySelector(".popup__save-button")
+let body = document.querySelector(".body");
+let popup = body.querySelector(".popup");
+let profilebutton = body.querySelector(".profile__info-edit");
+let closebutton = body.querySelector(".popup__close-button");
+let profileName = body.querySelector(".profile__name");
+let profileJob = body.querySelector(".profile__job");
+let nameInput = body.querySelector(".popup__name-input");
+let jobInput = body.querySelector(".popup__job-input");
+let formElement = body.querySelector(".popup__container");
+let saveButton = body.querySelector(".popup__save-button");
+let popupoverlay = body.querySelector(".popup__overlay");
 
-function openpopup(){
-popup.classList.add("popup_opened")
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
+function openpopup() {
+  popup.classList.add("popup__opened");
+  popupoverlay.classList.add("popup__overlay-opened")
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 profilebutton.addEventListener("click", openpopup);
 
-function closepopup(){
-popup.classList.remove("popup_opened")
+function closepopup() {
+  popup.classList.remove("popup__opened");
 }
 
 closebutton.addEventListener("click", closepopup);
